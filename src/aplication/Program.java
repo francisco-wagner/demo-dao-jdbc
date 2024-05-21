a2828e3441a232ca4a225d5ecad46a588054b31a
francisco-wagner <franciscowagner200@gmail.com>
2024-05-21 13:44:11 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,16 @@
 package aplication;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department departObj = new Department(1, "Books");
-        System.out.println(departObj);
+
+        Seller sellerObj = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, departObj);
+        System.out.println(sellerObj);
     }
 }
