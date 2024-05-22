c43f75ce4b1fb355a3c9c7d45a38627ad1a8a01f
francisco-wagner <franciscowagner200@gmail.com>
2024-05-22 13:43:45 -0300

    findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -107,6 +107,7 @@ public class Seller implements Serializable {
                 ", email='" + email + '\'' +
                 ", birthDate=" + birthDate +
                 ", baseSalary=" + baseSalary +
+                ", department=" + department +
                 '}';
     }
 }
